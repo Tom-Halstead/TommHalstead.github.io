787bddaf28f602af40109022702302867a0fcfe9
TommHalstead <110309181+TommHalstead@users.noreply.github.com>
2023-05-23 16:57:43 -0500

    including header on all pages

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,10 @@
 "use strict";
 
 const nav = document.querySelector(".nav");
-const navLink = document.querySelectorAll(".nav-btn");
+const header = document.querySelector("header");
 
 const stickyNav = () =>
   window.scrollY > window.innerHeight
     ? nav.classList.add("sticky")
     : nav.classList.remove("sticky");
-
 window.addEventListener(`scroll`, stickyNav);
