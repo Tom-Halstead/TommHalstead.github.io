747026be295156fe8c557127918118a69718f21e
TommHalstead <110309181+TommHalstead@users.noreply.github.com>
2023-05-24 22:54:03 -0500

    test on css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 "use strict";
 
 const logo = document.querySelector(".logo");
+// const headTest = document.querySelector(".headtest");
+// const navTest = (document.querySelector(".navtest").innerHTML = ``);
+
+// console.log(navTest);
+// console.log(headTest);
 
 logo.addEventListener(`mouseover`, function (e) {
   console.log("Hover");
---
+++ assets/js/test.js
@@ -0,0 +1,7 @@
+const header = document.createElement("header").classList.add(".headtest");
+
+const headTest = document.querySelector(".headtest");
+const navTest = (document.querySelector(".navtest").innerHTML = ``);
+
+console.log(navTest);
+console.log(headTest);
