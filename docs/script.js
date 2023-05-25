0d59dd17ca297ea3afb3f6bcd95484230b62c85e
TommHalstead <110309181+TommHalstead@users.noreply.github.com>
2023-05-25 18:43:48 -0500

    color changing

--- docs/script.js
+++ docs/script.js
@@ -1,10 +1,29 @@
 "use strict";
 
-const nav = document.querySelector(".nav");
-const header = document.querySelector("header");
+const logo = document.querySelector(".logo");
+const myName = document.querySelector(".name");
 
-const stickyNav = () =>
-  window.scrollY > window.innerHeight
-    ? nav.classList.add("sticky")
-    : nav.classList.remove("sticky");
-window.addEventListener(`scroll`, stickyNav);
+const randomColor = () => {
+  return (
+    `#` +
+    Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, "0")
+      .toUpperCase()
+  );
+};
+
+// const changeColor = function () {
+//   myName.style.color =
+//     `#` +
+//     Math.floor(Math.random() * 16777215)
+//       .toString(16)
+//       .padStart(6, "0")
+//       .toUpperCase();
+// };
+
+document.body.addEventListener(`click`, () => {
+  myName.style.color = `${randomColor()}`;
+});
+
+// setInterval(changeColor, 2000);
