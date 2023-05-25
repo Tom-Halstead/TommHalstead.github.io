073a9234e95f7a2ec158a6211221a1d3bd4c6a2d
TommHalstead <110309181+TommHalstead@users.noreply.github.com>
2023-05-24 23:01:27 -0500

    deleted cname content

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,7 +1,7 @@
-const header = document.createElement("header").classList.add(".headtest");
-
+const header = document.createElement("p");
+console.log(header);
 const headTest = document.querySelector(".headtest");
 const navTest = (document.querySelector(".navtest").innerHTML = ``);
 
-console.log(navTest);
+console.log(header);
 console.log(headTest);
